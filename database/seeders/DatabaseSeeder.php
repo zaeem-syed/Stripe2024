801a6a2ef9748143e2ccc453fcc729701a780652
zaeem syed <xaeem.ds@gmail.com>
2024-08-02 03:33:03 +0500

    added more info

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,7 +31,8 @@ class DatabaseSeeder extends Seeder
 
 
         // $this->call(CountrySeeder::class);
-        $this->call(CitySeeder::class);
+        // $this->call(CitySeeder::class);
+        $this->call(ShopSeeder::class);
 
 
 
--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Shop;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ShopSeeder extends Seeder
 {
@@ -13,5 +14,41 @@ class ShopSeeder extends Seeder
     public function run(): void
     {
         //
+        $city_ids=[1,2,3,4,5,6,7,8,9];
+        $shops = [
+            "The Golden Stitch Tailors",
+            "Emerald Elegance Jewelry",
+            "Bread & Butter Bakery",
+            "Ruby's Jewel Box",
+            "Savory Spice Kitchen",
+            "The Artisan Tailor",
+            "Pearl Paradise Jewelers",
+            "The Pie Haven",
+            "Diamond Dreams Boutique",
+            "The Stitch & Sew Tailor Shop",
+            "Luxe & Luster Jewelry",
+            "The Gourmet Pantry",
+            "Vintage Vogue Tailors",
+            "Sweet Tooth Confections",
+            "Sapphire Sparkle Jewels",
+            "Taste of Tradition Bakery",
+            "The Couture Closet Tailors",
+            "Gleaming Gems Jewelry",
+            "The Baker's Dozen"
+        ];
+
+        foreach($shops as $shop)
+        {
+             $city_id = $city_ids[array_rand($city_ids)];
+            Shop::create([
+                'name' => $shop,
+                'city_id' => $city_id 
+            ]);
+        }
+
+
+
+
+
     }
 }
